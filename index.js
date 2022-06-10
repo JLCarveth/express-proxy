38c615d3d3143e5dc80e46872a2d29ffdc6b8ba7
JLCarveth <jlcarveth@gmail.com>
2022-06-10 09:11:51 -0400

    dotenv

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ import fs from 'fs/promises';
 import express from 'express';
 import request from 'request'
 import bodyParser from 'body-parser';
+import {performance} from 'perf_hooks';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const LOGGING = (process.env.DO_LOGGING === 'true') ? true : false;
@@ -20,19 +24,20 @@ console.log('Using limit: ', myLimit);
 
 if (LOGGING) console.log("Logging enabled, logfile: " + filepath);
 
-function log(message) {
-    fs.appendFile(filepath, message, {
+function log(message, level) {
+    let date = new Date(Date.now()).toISOString();
+    let line = `${date} | ${level} | ${message}`;
+    fs.appendFile(filepath, line, {
         'flag' : 'a+'
-    }, (error) => {
-        console.error("ERROR: Cannot write to logfile.");
-        console.error(JSON.stringify(error));
+    }, (err) => {
+        console.error("ERROR: Cannot write to logfile.\n", JSON.stringify(err));
     });
 }
 
 app.use(bodyParser.json({ limit: myLimit }));
 
-app.all('*', function (req, res, next) {
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+app.all('*', function (req, res) {
+    const t0 = performance.now();
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');
     res.header('Access-Control-Allow-Headers', req.header('access-control-request-headers'));
@@ -45,19 +50,33 @@ app.all('*', function (req, res, next) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
+        /* Log incoming request */
         if (LOGGING) {
+            let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
             log(
-                'Target-URL:\t' + targetURL + '\n' +
-                'method:\t\t' + req.method + '\n' +
-                'Request Body:\t' + JSON.stringify(req.body) + '\n' +
-                'Headers:\t' + JSON.stringify(req.headers) + '\n' +
-                '*'.repeat(80) + '\n'
+                'Incoming Request:\n' +
+                '\tClient Address:\t' + ip +'\n' +
+                '\tTarget-URL:\t' + targetURL + '\n' +
+                '\tmethod:\t\t' + req.method + '\n' +
+                '\tRequest Body:\t' + JSON.stringify(req.body) + '\n' +
+                '\tHeaders:\t' + JSON.stringify(req.headers) + '\n',
+                'info' // Log Level
             );
         }
         request({ url: targetURL, method: req.method, json: req.body, headers: { 'Authorization': req.header('Authorization') } },
-            function (error, response, body) {
+            function (error, response) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
+                }
+                if (LOGGING) {
+                    const t1 = performance.now();
+                    log(
+                        'Request Response' + '\n' +
+                        '\tStatus Code:\t' + response.statusCode + '\n' +
+                        '\tElapsed Time:\t' + (t1 - t0).toFixed(2) + 'ms' + '\n' +
+                        '\tResponse Body:\t' + JSON.stringify(response.body) + '\n',
+                        'info' // Log Level
+                    );
                 }
             }).pipe(res);
     }
