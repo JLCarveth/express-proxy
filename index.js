238966742d9cb6538ce8ccfab3045f7fda8571e0
JLCarveth <jlcarveth@gmail.com>
2022-06-14 09:06:15 -0400

    Logging changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
  *  - Set a 'Target-URL' header pointing to API you want to access
  *  - Set body params, authentication if needed
  */
-import fs from 'fs/promises';
 import express from 'express';
 import request from 'request'
 import bodyParser from 'body-parser';
@@ -15,25 +14,14 @@ import {performance} from 'perf_hooks';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
 const LOGGING = (process.env.DO_LOGGING === 'true') ? true : false;
-const filepath = (process.env.LOG_FILE !== undefined) ? process.env.LOG_FILE : 'spire-inventory.log';
 
-var myLimit = typeof (process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+import logger from './log.js';
+const Logger = new logger();
+const app = express();
+
+var myLimit = typeof (process.env.LIMIT) != 'undefined' ? process.env.LIMIT : '250kb';
 console.log('Using limit: ', myLimit);
-
-if (LOGGING) console.log("Logging enabled, logfile: " + filepath);
-
-function log(message, level) {
-    let date = new Date(Date.now()).toISOString();
-    let line = `${date} | ${level} | ${message}`;
-    fs.appendFile(filepath, line, {
-        'flag' : 'a+'
-    }, (err) => {
-        console.error("ERROR: Cannot write to logfile.\n", JSON.stringify(err));
-    });
-}
-
 app.use(bodyParser.json({ limit: myLimit }));
 
 app.all('*', function (req, res) {
@@ -45,24 +33,24 @@ app.all('*', function (req, res) {
         // CORS Preflight
         res.send();
     } else {
-        var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
+        var targetURL = req.header('Target-URL');
         if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            res.send(500, { error: 'There is no Target-URL header in the request' });
             return;
         }
         /* Log incoming request */
         if (LOGGING) {
             let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-            log(
-                'Incoming Request:\n' +
-                '\tClient Address:\t' + ip +'\n' +
-                '\tTarget-URL:\t' + targetURL + '\n' +
-                '\tmethod:\t\t' + req.method + '\n' +
-                '\tRequest Body:\t' + JSON.stringify(req.body) + '\n' +
-                '\tHeaders:\t' + JSON.stringify(req.headers) + '\n',
-                'info' // Log Level
-            );
+            let data = {
+                "clientAddress" : ip,
+                "targetURL" : targetURL,
+                "method" : req.method,
+                "body": req.body,
+                "headers" : req.headers
+            };
+            Logger.log({'message' : 'Inbound request'}, data);
         }
+
         request({ url: targetURL, method: req.method, json: req.body, headers: { 'Authorization': req.header('Authorization') } },
             function (error, response) {
                 if (error) {
@@ -70,13 +58,13 @@ app.all('*', function (req, res) {
                 }
                 if (LOGGING) {
                     const t1 = performance.now();
-                    log(
-                        'Request Response' + '\n' +
-                        '\tStatus Code:\t' + response.statusCode + '\n' +
-                        '\tElapsed Time:\t' + (t1 - t0).toFixed(2) + 'ms' + '\n' +
-                        '\tResponse Body:\t' + JSON.stringify(response.body) + '\n',
-                        'info' // Log Level
-                    );
+                    let data = {
+                        "statusCode" : response.statusCode,
+                        "elapsedTime" : (t1 - t0).toFixed(2) + 'ms',
+                        "response" : response.body
+                    };
+                    
+                    Logger.log({'message' : 'Inbound response.'}, data);
                 }
             }).pipe(res);
     }
